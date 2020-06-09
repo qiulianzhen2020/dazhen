16eda5eab5eb8abc1dd3a98c899851ea0bf1932a
chenhuijuan2020 <1594961655@qq.com>
2020-06-09 17:51:10 +0800

    提交到本次仓库

---
+++ gitproject/src/com/git/dao/UserDao.java
@@ -0,0 +1,20 @@
+package com.git.dao;
+
+import java.util.List;
+
+public class UserDao {
+	public void addUser(int id,int username,String password) {
+		//add
+	}
+	public void deleteUser(int id) {
+		//delete
+	}
+	public void updateUser(int id,int username,String password) {
+		//update
+	}
+	public List<Object>scanUser(int pageIndex,int pageSize) {
+		//select
+		return null;
+	}
+
+}
